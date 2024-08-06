b323f81945e52afd238a7f2613ec50d799ba7d31
Jose <Josey@duck.com>
2024-08-06 16:35:16 -0400

    Added new functionality to this project

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo.Product(Product, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
