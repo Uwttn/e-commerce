e32f2b75680825edbe70b516e7c82b7d80b6d1ab
Jose <Josey@duck.com>
2024-08-07 10:30:46 -0400

    Added requests for ids for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,15 +16,16 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagData = await Tag.findByPk(req.params.id, {
+    const tagData = await Tag.findOne({where:{id:req.params.id},
       include: [{model: Product, through: ProductTag}]
     });
     if (!tagData) {
       res.status(404).json({message: 'No tag found with this product id'})
       return;
     }
-    res.status(200).json(productTagData);
+    res.status(200).json(tagData);
   } catch (err) {
+    console.log(err)
     res.status(500).json(err);
   }
   // be sure to include its associated Product data
